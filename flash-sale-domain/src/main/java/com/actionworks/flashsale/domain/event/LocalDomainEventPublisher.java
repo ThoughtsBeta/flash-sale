89280d5c8faa9d2442f2f0d8d4203c7e34d53d25
ThoughtsBeta <91445042+ThoughtsBeta@users.noreply.github.com>
2021-11-02 17:59:21 +0800

    Optimize comments.

--- flash-sale-domain/src/main/java/com/actionworks/flashsale/domain/event/LocalDomainEventPublisher.java
+++ flash-sale-domain/src/main/java/com/actionworks/flashsale/domain/event/LocalDomainEventPublisher.java
@@ -8,7 +8,7 @@ import javax.annotation.Resource;
 
 /**
  * 发布领域事件，一般基于独立的消息中间件系统发布；
- * 为方便示例，本系统采用本地时间发布
+ * 为方便示例，本系统采用本地事件发布。
  */
 @Component
 public class LocalDomainEventPublisher implements DomainEventPublisher {
