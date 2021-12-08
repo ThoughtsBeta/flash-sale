f2ef8e725dc0ca6cda6dd4fc244772ed52a3345b
ThoughtsBeta <thoughts.beta@gmail.com>
2021-12-08 13:20:58 +0800

    日志中增加IP字段

---
+++ flash-sale-infrastructure/src/main/java/com/actionworks/flashsale/util/IPConverterConfig.java
@@ -0,0 +1,20 @@
+package com.actionworks.flashsale.util;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IPConverterConfig extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent iLoggingEvent) {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
